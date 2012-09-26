d169592c7e0f571a6f1f835b73e12396799eaf5e
kosowsjm195 <kosowsjm195@potsdam.edu>
2012-09-26 18:50:54 -0400

    Player class now sets the starting location in constructor.
    When the constructor is called and passes in 1 as the only parameter, the
    location of that player is automatically set to (5,0).  Player 2 will
    be set to (5,9).  And anything else will default the location to (0,0).

--- src/player/Player.java
+++ src/player/Player.java
@@ -5,9 +5,16 @@ import java.awt.Dimension;
 public class Player {
 
 	private Dimension xy;
+	private int playerNumber;
 	
 	public Player() {
-		xy = new Dimension(0,0);
+		playerNumber = 0;
+		setStartingLocation();
+	}
+	
+	public Player(int n) {
+		playerNumber = n;
+		setStartingLocation();
 	}
 	
 	public void setLocation(int i, int j) {
@@ -21,6 +28,21 @@ public class Player {
 
 	public Dimension getLocation() {
 		return xy;
+	}
+	
+	private void setStartingLocation() {
+		if (playerNumber == 1) {
+			xy = new Dimension(5,0);
+		}else if (playerNumber == 2) {
+			xy = new Dimension(5,9);
+		}else {
+				xy = new Dimension(0,0);
+		}
+	}
+	
+	private Dimension getStartingLocation() {
+		Dimension d = new Dimension(5, 0);
+		return d;
 	}
 
 	
