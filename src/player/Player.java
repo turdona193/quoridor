ec3da63c379a6adcc46015973c800c3c764a4eea
kosowsjm195 <kosowsjm195@potsdam.edu>
2012-09-27 14:36:17 -0400

    Player class tweaked some more.
    Now uses ints x and y to represent location instead of Dimension object.
    Should also now keep track of number of walls each player has left to place.

--- src/player/Player.java
+++ src/player/Player.java
@@ -4,46 +4,77 @@ import java.awt.Dimension;
 
 public class Player {
 
-	private Dimension xy;
-	private int playerNumber;
+	private int x, y;
+	private int playerID;
+	private int walls;
 	
 	public Player() {
-		playerNumber = 0;
+		playerID = 0;
 		setStartingLocation();
 	}
 	
-	public Player(int n) {
-		playerNumber = n;
+	public Player(int ID) {
+		playerID = ID;
 		setStartingLocation();
 	}
 	
-	public void setLocation(int i, int j) {
-		xy.setSize(i, j);
+	public Player(int ID, int NumOfPlayers) {
+		playerID = ID;
+		setStartingLocation();
+		setStartingWalls(NumOfPlayers);
 		
 	}
 	
-	public void setLocation(Dimension d) {
-		xy.setSize(d);
+	private void setStartingWalls(int numOfPlayers) {
+		if (numOfPlayers == 2) {
+			walls = 10;
+		}else if (numOfPlayers == 4) {
+			walls = 5;
+		}else {
+			walls = 0;
+		}
+		
+	}
+	
+	public int getWalls() {
+		return walls;
+	}
+	
+	public void placeWall() {
+		walls--;
 	}
 
-	public Dimension getLocation() {
-		return xy;
+	public void setLocation(int i, int j) {
+		x = i;
+		y = j;
+		
+	}
+
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
 	}
 	
 	private void setStartingLocation() {
-		if (playerNumber == 1) {
-			xy = new Dimension(5,0);
-		}else if (playerNumber == 2) {
-			xy = new Dimension(5,9);
+		if (playerID == 1) {
+			x = 5;
+			y = 0;
+		}else if (playerID == 2) {
+			x = 5;
+			y = 9;
+		}else if (playerID == 3) {
+			x = 0;
+			y = 5;
+		}else if (playerID == 4) {
+			x = 9;
+			y = 5;
 		}else {
-				xy = new Dimension(0,0);
+				x = 0;
+				y = 0;
 		}
 	}
-	
-	private Dimension getStartingLocation() {
-		Dimension d = new Dimension(5, 0);
-		return d;
-	}
-
 	
 }
--- tst/player_test/PlayerTest.java
+++ tst/player_test/PlayerTest.java
@@ -14,32 +14,29 @@ import player.Player;
 public class PlayerTest {
 	private Player pl;
 		
-	/*@Before
-	  public void initialize() {
-	    pl = new Player();
-	  }*/  
+	private void testXAndYValues(int expectX, int expectY) {
+		assertThat(expectX, is(equalTo(pl.getX())));
+		assertThat(expectY, is(equalTo(pl.getY())));	
+	}
 	
 	@Test
 	public void testGetAndSetLocation() {
 		pl = new Player();
 		pl.setLocation(5, 5);
-		Dimension d = new Dimension(5, 5);
-		assertThat(d, is(equalTo(pl.getLocation())));
+		int expectedX = 5;
+		int expectedY = 5;
+		testXAndYValues(expectedX, expectedY);
 		
 	}
 	
 	@Test
-	public void testStartingLocationOfP1() {
-		pl = new Player(1);
-		Dimension d = new Dimension(5, 0);
-		assertEquals(d, pl.getLocation());
+	public void testStartingLocationsOfPlayers() {
+		int expectedX[] = {5, 5, 0, 9};
+		int expectedY[] = {0, 9, 5, 5};
+		for (int i = 0; i < expectedX.length; i++) {
+			pl = new Player(i+1);
+			testXAndYValues(expectedX[i], expectedY[i]);
+		}
 	}
-	
-	@Test
-	public void testStartingLocationOfP2() {
-		pl = new Player(2);
-		Dimension d = new Dimension(5, 9);
-		assertEquals(d, pl.getLocation());	
-	}
-	
+		
 }
