e6efe19f56983e0d7900234b8d64de0766d13b63
Andrew Allen Barkley <barkle36@gmail.edu>
2012-11-28 14:03:43 -0500

    AIs move toward their goal but won't jump

--- src/ai/AI.java
+++ src/ai/AI.java
@@ -1,115 +1,73 @@
 package ai;
 
 import java.awt.Point;
+import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.Semaphore;
 
-import javax.swing.JButton;
-
 import main.Quoridor;
 import main.QBoard;
-
-/*import com.objogate.wl.swing.AWTEventQueueProber;
-import com.objogate.wl.swing.driver.ComponentDriver;
-import com.objogate.wl.swing.driver.JButtonDriver;
-import com.objogate.wl.swing.driver.JFrameDriver;
-import com.objogate.wl.swing.driver.JRadioButtonDriver;
-import com.objogate.wl.swing.gesture.GesturePerformer;*/
-
-//import util.Graph;
 import player.Board;
+import player.GameState;
+import util.Graph;
 
+/**
+ * AI implements an artificial intelligence for the game.
+ */
 public class AI extends Thread {
 
-    //private Graph<Point> graph;
+    /**
+     * Default search algorithm to use when searching for paths.
+     */
+    private static final String DEFAULT_SEARCH = "breadth-first";
+
     private Board board;
     private Semaphore sem;
-    //JFrameDriver qBoardDriver;
+    private GameState state;
 
-    //public AI(Graph<Point> graph, Board board) {
+    /**
+     * Creates a new AI.
+     *
+     * @param board
+     *     the board for this game
+     *
+     * @param sem
+     *     the semaphor used by this AI
+     */
     public AI(Board board, Semaphore sem) {
-        //this.graph = graph;
         this.board = board;
         this.sem = sem;
-        //qBoardDriver = new JFrameDriver(new GesturePerformer(), new AWTEventQueueProber(), JFrameDriver.named(qBoard.BOARD_WINDOW_TITLE), JFrameDriver.showingOnScreen());
     }
 
+    /**
+     * Makes a move along the shortest path to the goal for the current player.
+     */
     public void makeMove() {
-        print("Enter an AI move: ");
-        Scanner in = new Scanner(System.in);
-        board.readStringFromGUI(in.nextLine());
+        List<Point>  path;
+        Graph<Point> graph;
+        Point        nextLocation;
+        String       move;
+
+        state = board.getCurrentState();
+        graph = state.getGraph();
+        path  = graph.findPath(DEFAULT_SEARCH,
+                               state.getCurrentPlayerLocation(),
+                               state.getCurrentPlayerGoalSet());
+        nextLocation = path.get(1);
+        move = "M " + nextLocation.x + " " +
+                      nextLocation.y;
+        if (board.isStringLegal(move)) {
+            board.readStringFromGUI(move);
+        }
     }
     
-    /*private JButtonDriver button(String name) {
-        return new JButtonDriver(qBoardDriver, JButton.class, ComponentDriver.named(name));
-	}*/
-    
-    //Method to make a random move, doesn't look as cool as I thought it would be.
-    private void randomAIMove() throws InterruptedException {
-    	//sleep(100);
-    	if (board.hasWalls(board.getTurn())) {
-        	int MoveOrWall = (int)(Math.random()*2);
-        	if (MoveOrWall == 0)
-        		makeRandomMove();
-        	if (MoveOrWall == 1)
-        		placeRandomWall();
-    	}
-    	else
-    		makeRandomMove();
-
-    }
-
-    //Keeps picking a random location and orientation until it finds a legal move, then makes it.
-    private void placeRandomWall() {
-    	while(true) {
-    		int VertOrHori = (int)(Math.random()*2);
-    		int x = (int)(Math.random()*8);
-			int y = (int)(Math.random()*8);
-			String str = "";
-        	if (VertOrHori == 0)
-        		str = ("H" + " " + x + " " + y);
-        	if (VertOrHori == 1)
-        		str = ("V" + " " + x + " " + y);
-        	
-        	if(board.isStringLegal(str)) {
-        		board.readStringFromGUI(str);
-        		//JButtonDriver bDriver = button(str);
-       			//bDriver.click();
-				return;
-			}       	
-    	}
-	}
-
-    //Keeps picking a random location until a legal move has been found, then makes it.
-	private void makeRandomMove() {
-		while(true) {
-			int x = (int)(Math.random()*9);
-			int y = (int)(Math.random()*9);
-			String str = ("M" + " " + x + " " + y);
-			if(board.isStringLegal(str)) {
-				board.readStringFromGUI(str);
-				//JButtonDriver bDriver = button(str);
-       			//bDriver.click();
-				return;
-			}
-		}
-		
-	}
-
-	private void print(String s) {
-        System.out.print(s);
-    }
-    
+    /**
+     * Acquires a semaphor and waits for a release to make a move.
+     */
     public void run() {
     	while (true) {
     		sem.acquireUninterruptibly();
-    		//makeMove();
-    		try {
-				randomAIMove();
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+    		makeMove();
     	}
     }
 }
